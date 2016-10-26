133a37295f130b0c225634cfdece0492995838f3
Jni <jni.viens@gmail.com>
2016-10-26 14:31:17 -0400

    Adds rest assured tests

---
+++ src/test/java/ca/ulaval/glo4002/atm/contexts/TestContext.java
@@ -0,0 +1,42 @@
+package ca.ulaval.glo4002.atm.contexts;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import ca.ulaval.glo4002.atm.application.ServiceLocator;
+import ca.ulaval.glo4002.atm.application.banking.BankingService;
+import ca.ulaval.glo4002.atm.application.jpa.EntityManagerFactoryProvider;
+import ca.ulaval.glo4002.atm.application.jpa.EntityManagerProvider;
+import ca.ulaval.glo4002.atm.domain.accounts.AccountRepository;
+import ca.ulaval.glo4002.atm.domain.accounts.StandardAccount;
+import ca.ulaval.glo4002.atm.domain.dispensers.CashDispenser;
+import ca.ulaval.glo4002.atm.infrastructure.dispensers.FakeCashDispenser;
+import ca.ulaval.glo4002.atm.infrastructure.persistence.HibernateAccountRepository;
+
+public class TestContext implements Context {
+
+    @Override
+    public void apply() {
+        ServiceLocator.registerSingleton(AccountRepository.class, new HibernateAccountRepository());
+        ServiceLocator.registerSingleton(CashDispenser.class, new FakeCashDispenser());
+        ServiceLocator.registerSingleton(BankingService.class, new BankingService());
+
+        fillDatabase();
+    }
+
+    private void fillDatabase() {
+        EntityManagerFactory entityManagerFactory = EntityManagerFactoryProvider.getFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityManagerProvider.setEntityManager(entityManager);
+
+        AccountRepository accountRepository = new HibernateAccountRepository();
+
+        entityManager.getTransaction().begin();
+        accountRepository.persist(new StandardAccount(123, 1000));
+        accountRepository.persist(new StandardAccount(456, 1));
+        entityManager.getTransaction().commit();
+
+        EntityManagerProvider.clearEntityManager();
+        entityManager.close();
+    }
+}
---
+++ src/test/java/ca/ulaval/glo4002/atm/rest/RestTestSuite.java
@@ -0,0 +1,32 @@
+package ca.ulaval.glo4002.atm.rest;
+
+import ca.ulaval.glo4002.atm.AtmServer;
+import ca.ulaval.glo4002.atm.contexts.DevContext;
+import ca.ulaval.glo4002.atm.contexts.TestContext;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ WithdrawResourceRestTest.class })
+public class RestTestSuite {
+
+    public static final int TEST_SERVER_PORT = 9292;
+
+    private static AtmServer atmServer;
+
+    @BeforeClass
+    public static void setUpClass() {
+        atmServer = new AtmServer();
+        atmServer.start(TEST_SERVER_PORT, new TestContext());
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        atmServer.stop();
+    }
+
+}
