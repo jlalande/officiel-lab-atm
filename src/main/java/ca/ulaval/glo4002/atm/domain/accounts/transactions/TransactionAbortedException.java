ebb58493ee6c4b15fbd8a5e5fb3993a7087015ee
Jni <jni.viens@gmail.com>
2018-11-14 02:38:12 -0500

    Removes useless ctor

--- src/main/java/ca/ulaval/glo4002/atm/domain/accounts/transactions/TransactionAbortedException.java
+++ src/main/java/ca/ulaval/glo4002/atm/domain/accounts/transactions/TransactionAbortedException.java
@@ -1,14 +1,4 @@
 package ca.ulaval.glo4002.atm.domain.accounts.transactions;
 
 public class TransactionAbortedException extends RuntimeException {
-
-    public TransactionAbortedException() {
-    }
-
-    public TransactionAbortedException(Throwable cause) {
-        super(cause);
-    }
-
-    private static final long serialVersionUID = 1L;
-
 }
